e012b40bf80c6ae537d855aaa68aa9b964bfdfea
Linuccia <polina.stephanskaya@gmail.com>
2021-06-05 00:56:20 +0300

    MBean added

--- src/main/java/main/mBeans/HitPercentage.java
+++ src/main/java/main/mBeans/HitPercentage.java
@@ -6,14 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class HitPercentage implements HitPercentageMBean{
     private double percent;
-    private int allPoints;
-    private int inArea;
+    private double allPoints;
+    private double inArea;
 
     public void countPercentage(Point point) {
         allPoints++;
         if (point.getHit()) inArea++;
-        if (allPoints != 0) percent = inArea/allPoints * 100;
-        else percent = 0.0;
+        percent = inArea/allPoints * 100;
     }
 
     @Override
